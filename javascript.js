8fdd3e37fcf1e35e25b2a1933879c9dbada479fe
Alasdair James <alasdair.marchant@gmail.com>
2019-01-05 14:19:54 +0000

    Added functions for DRYer code
    Added 3 seperate functions, addListItem, keyPress, inputPress.
    I then called the functions

--- javascript.js
+++ javascript.js
@@ -7,27 +7,37 @@ var input2 = document.querySelector(".input2");
 var addBtn1 = document.querySelector(".addBtn1");
 var randomizeBtn = document.querySelector(".randomizeBtn");
 
+
+//functino to which we pass the functions 
+
 // add a list item to the first dataset 
-function addListItem(item) {
+function addListItem(item, dataset) {
 	var listItem = document.createElement("li");
 	listItem.textContent = item;
-	dataset1.appendChild(listItem)
+	dataset.appendChild(listItem)
 }
 
-// call the add item function
-//using jquery to detect enter press within the input box
-$(input1).on("keypress", function(e){
-	if(e.which == 13) {
-		var testInput = input1.value;
-		console.log(testInput);
+// a function whose paramaters are the input and dataset
+function keyPress(input, dataset) {
+		var boxInput = input.value;
+		console.log(boxInput);
 		//check if user has entered text
-		if (testInput === "") {
+		if (boxInput === "") {
 			alert("Please write text!");
 		//if they have, add it to the list by calling function
 		} else {
-			addListItem(testInput);
+			addListItem(boxInput, dataset);
 			//clear the box
-			input1.value = "";
+			input.value = "";
 		}
-}});
+}
 
+//using jquery to detect enter press within the input box
+function inputPress(input, dataset) {
+	$(input).on("keypress", function(e) {
+	if(e.which == 13) {
+		keyPress(input,dataset);
+}})};
+
+inputPress(input1,dataset1);
+inputPress(input2,dataset2);
